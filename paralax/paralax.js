c7f5915def61b01c8778aa9b2bec7a1b4f818398
Nate Mara <natemara@gmail.com>
2014-12-11 17:47:11 -0500

    make instance variables

--- paralax/paralax.js
+++ paralax/paralax.js
@@ -2,19 +2,59 @@ var Paralax = function(leftCanvas, rightCanvas) {
 	this.left = document.getElementById(leftCanvas).getContext("2d");
 	this.right = document.getElementById(rightCanvas).getContext("2d");
 
+	this.canvasWidth =  window.innerWidth/2;
+	this.canvasHeight = screen.height;
+
 	//Geometric Model Variables
-	this.a = canvasWidth/2;
-	this.c = canvasHeight/2;
-	this.b = a / Math.tan(Paralax.FOV/2);
-	this.theta0 = Math.atan(b/a);
-	this.theta = Math.PI - (2*theta0);
-	this.theta1 = Math.atan(b/c);
-	this.thetaY = Math.PI - (2*theta1);
+	this.a = this.canvasWidth/2;
+	this.c = this.canvasHeight/2;
+	this.b = this.a / Math.tan(Paralax.FOV/2);
+	this.theta0 = Math.atan(this.b / this.a);
+	this.theta = Math.PI - (2 * this.theta0);
+	this.theta1 = Math.atan(this.b / this.c);
+	this.thetaY = Math.PI - (2 * this.theta1);
+
+	//Test variables (object variables)
+	this.x = [0,0,0];
+	this.y = [0,0,0];
+	this.z = [0,0,0];
 
 	//Test Model Variables
 	this.count=0;
 
-	this.timer = setInterval(this.run,1000/Paralax.FPS);;
+	this.shapes = [];
+
+	this.run = function() {
+		console.log(this.left);
+		this.left.fillStyle = "#FFFFFF";
+		this.right.fillStyle = "#FFFFFF";
+		this.left.fillRect(0,0,this.canvasWidth,this.canvasHeight);
+		this.right.fillRect(0,0,this.canvasWidth,this.canvasHeight);
+
+		this.left.fillStyle = "#000000";
+		this.right.fillStyle = "#000000";
+
+		for(var i = 0;i<x.length;i++){
+			//Prints all the of spheres using arrays and the screen position functions (the )
+			this.left.beginPath();
+			this.left.arc(leftX(x[i],y[i],z[i]),leftY(x[i],y[i],z[i]),findRadius(x[i],y[i],z[i],30),0,2*Math.PI);
+			this.left.fill();
+
+			this.right.beginPath();
+			this.right.arc(rightX(x[i],y[i],z[i]),rightY(x[i],y[i],z[i]),findRadius(x[i],y[i],z[i],30),0,2*Math.PI);
+			this.right.fill();
+		}
+
+
+		x = [300*Math.cos(count),300*Math.cos(count),-300*Math.cos(count),-300*Math.cos(count),0];
+		y = [300*Math.cos(count),-300*Math.cos(count),300*Math.cos(count),-300*Math.cos(count),0];
+		z = [500 + 300*Math.sin(count),500 + 300*Math.sin(count),500 - 300*Math.sin(count),500 - 300*Math.sin(count),500];
+
+		count+=.07;
+	}
+
+	var that = this
+	this.timer = setInterval(function() {return that.run.apply(that);}, 1000/Paralax.FPS);
 } || {};
 
 Paralax.toRadians = function (angle) {
@@ -26,56 +66,6 @@ Paralax.FPS = 30;
 
 // Field of vision in radians
 Paralax.FOV = Paralax.toRadians(120);
-
-//Geometric Model Constants
-var theta,thetaY,theta0,theta1,a,b,c;
-
-//various variables
-var canvasHeight,canvasWidth,timer;
-
-//Test variables (object variables)
-var count;
-var x = [0,0,0];
-var y = [0,0,0];
-var z = [0,0,0];
-
-Paralax.start = function(){
-}
-
-
-Paralax.run = function() {
-	this.left.fillStyle = "#FFFFFF";
-	this.right.fillStyle = "#FFFFFF";
-	this.left.fillRect(0,0,canvasWidth,canvasHeight);
-	this.right.fillRect(0,0,canvasWidth,canvasHeight);
-
-	this.left.fillStyle = "#000000";
-	this.right.fillStyle = "#000000";
-
-	for(var i = 0;i<x.length;i++){
-		//Prints all the of spheres using arrays and the screen position functions (the )
-		this.left.beginPath();
-		this.left.arc(leftX(x[i],y[i],z[i]),leftY(x[i],y[i],z[i]),findRadius(x[i],y[i],z[i],30),0,2*Math.PI);
-		this.left.fill();
-
-		this.right.beginPath();
-		this.right.arc(rightX(x[i],y[i],z[i]),rightY(x[i],y[i],z[i]),findRadius(x[i],y[i],z[i],30),0,2*Math.PI);
-		this.right.fill();
-	}
-
-
-	x = [300*Math.cos(count),300*Math.cos(count),-300*Math.cos(count),-300*Math.cos(count),0];
-	y = [300*Math.cos(count),-300*Math.cos(count),300*Math.cos(count),-300*Math.cos(count),0];
-	z = [500 + 300*Math.sin(count),500 + 300*Math.sin(count),500 - 300*Math.sin(count),500 - 300*Math.sin(count),500];
-
-	count+=.07;
-
-	if(z<0) {
-		clearInterval(timer);
-	}
-}
-
-
 
 //Screen Position Functions in Terms of 3D Position
 function leftX(x,y,z){
