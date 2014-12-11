05ace6e1af4be0446ea56f2c1ec59a5fd4887af1
Nate Mara <natemara@gmail.com>
2014-12-11 16:38:25 -0500

    remove unused sleep function

--- paralax/paralax.js
+++ paralax/paralax.js
@@ -94,16 +94,6 @@ function findRadius(x,y,z,realRadius){
 	return leftY(x,y-realRadius,z)-leftY(x,y,z);
 }
 
-function sleep(milliseconds) {
-	var start = new Date().getTime();
-	for (var i = 0; i < 1e7; i++) {
-		if ((new Date().getTime() - start) > milliseconds){
-			break;
-		}
-	}
-	return 0;
-}
-
 function Sphere(x, y, z) {
 	this.x = x;
 	this.y = y;
