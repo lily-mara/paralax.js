661f6e60e2790431e1d4740f315f5b76b9f45181
Nate Mara <natemara@gmail.com>
2014-12-11 22:51:52 -0500

    update site

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 var p;
+var shape;
+var mouse = {};
+
+$(document).mousemove(function( event ) {
+	mouse.x = event.pageX;
+	mouse.y = event.pageY;
+});
+
+document.onkeydown = reactKey;
+
+function reactKey(evt) {
+	if(evt.keyCode == 40) {
+		shape.z -= 10;
+	} else if (evt.keyCode == 38) {
+		shape.z += 10;
+	} else if (evt.keyCode == 37) {
+		shape.x -= 10;
+	} else if (evt.keyCode == 39) {
+		shape.x += 10;
+	}
+}
 
 function goFullScreen(){
 	document.getElementById('button').style.display="none";
@@ -49,6 +70,11 @@ function goFullScreen(){
 		this.y = -300*Math.cos(count);
 		this.z = 500 - 300*Math.sin(count);
 	}, p));
-	p.shapes.push(new Sphere(0, 0, 500, function(count) {
+	shape = new Sphere(0, 0, 500, function(count) {}, p);
+	p.shapes.push(new Sphere(0, 0, 0, function(count) {
+		this.x = mouse.x;
+		this.y = mouse.y;
+		this.z = 500 - 300*Math.sin(count);
 	}, p));
+	p.shapes.push(shape);
 }
